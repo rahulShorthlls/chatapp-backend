87e1457381c4b3da7bf98e2837715cfec7fe8a64
rahul <rahul.kumar2@shorthills.ai>
2025-05-04 19:34:18 +0530

    shdhdd

--- server.js
+++ server.js
@@ -29,12 +29,15 @@ app.use(cors({
 app.use(express.json());
 
 let messages = [];
+let users = {}; // Track connected users
 
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
     socket.on('new-user', (name) => {
+        users[socket.id] = name;
         console.log(`${name} has joined the chat.`);
+        io.emit('user-connected', { id: socket.id, name });
     });
 
     socket.on('send-message', (data) => {
@@ -47,10 +50,14 @@ io.on('connection', (socket) => {
         console.log('Reply received:', data);
         const replyMessage = {
             ...data,
-            replyTo: data.replyTo // Include the ID or content of the message being replied to
+            replyTo: data.replyTo
         };
         messages.push(replyMessage);
         io.emit('receive-reply', replyMessage);
+    });
+
+    socket.on('typing', (data) => {
+        io.emit('user-typing', data); // Broadcast typing status
     });
 
     socket.on('clear-chat', () => {
@@ -60,11 +67,14 @@ io.on('connection', (socket) => {
     });
 
     socket.on('mark-seen', (data) => {
-        io.emit('message-seen', data);
+        io.emit('message-seen', data); // Notify all users about read receipts
     });
 
     socket.on('disconnect', () => {
         console.log('A user disconnected:', socket.id);
+        const user = users[socket.id];
+        delete users[socket.id];
+        io.emit('user-disconnected', { id: socket.id, name: user });
     });
 });
 
